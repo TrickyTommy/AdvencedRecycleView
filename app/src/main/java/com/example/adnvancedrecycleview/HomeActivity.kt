590743874d56834d92a5d8ead25c337300a1585e
trickytommy <trickytommy4@gmail.com>
2020-11-21 11:49:07 +0700

    wip

--- app/src/main/java/com/example/adnvancedrecycleview/HomeActivity.kt
+++ app/src/main/java/com/example/adnvancedrecycleview/HomeActivity.kt
@@ -2,8 +2,17 @@ package com.example.adnvancedrecycleview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.adnvancedrecycleview.databinding.FragmentHomeActivityBinding
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+
 
 class HomeActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: FragmentActivity
+    //private val adapter by lazy { UserAdapter(requireContext(), this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
